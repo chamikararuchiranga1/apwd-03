7bf8ddf85091018ffb85a1e70363854a94050369
chami <chamikararuchiranga1@gmail.com>
2023-11-08 22:07:08 +0530

    document write js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -15,5 +15,28 @@ document.write("\n\n");
 document.write('<br>')
 document.write('<h1>Colombo</h1>')
 document.write('<button>Click</button>')
+document.write('<br><br>')
+document.write('<h3>Chamikara Ruchiranga <br> 29 <br> Panadura</h3>')
+// document.write('<h3>Rchiranga</h3>')
 
+// global Variable 
+var name = 'Chamikara';
 
+// local variable 
+{
+    var town = "Panadura";
+}
+
+// task 
+// Full Name
+// Age 
+// Town 
+
+// data types 
+// Numbers e.g. 112, 67, 87.98
+
+// String 
+// "Chamikara"
+
+// Boolean 
+// true / false
