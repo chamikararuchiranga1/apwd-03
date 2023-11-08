8d4baf18f9b7bfdef61e0ee53a1ed9776c4ffbce
chami <chamikararuchiranga1@gmail.com>
2023-11-08 21:29:25 +0530

    start java script

---
+++ JavaScript/index.js
@@ -0,0 +1,19 @@
+console.log("AFSD");
+
+// console.log("Chamikara Ruchiranga");
+
+// console.log("Chamikara Ruchiranga");
+// console.log("Chamikara Ruchiranga");
+// console.log("Chamikara Ruchiranga");
+console.log("Chamikara Ruchiranga");
+
+// Alert
+// alert('Chamikara');
+
+document.write('Panadura')
+document.write("\n\n");
+document.write('<br>')
+document.write('<h1>Colombo</h1>')
+document.write('<button>Click</button>')
+
+
